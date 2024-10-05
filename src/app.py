e99efebda13bfeafa6d16082bff93bb7bcda1f51
José Jaramillo <josejara93106@gmail.com>
2024-10-05 03:35:21 +0000

    POST and DELETE a un usuario especifico

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, FavoritePlanets, FavoritePeople
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,100 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    all_users = User.query.all()
+    results = list(map(lambda usuario: usuario.serialize(),all_users)) 
+    return jsonify(results), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user_id(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify(user.serialize()), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    all_people = People.query.all()
+    results = list(map(lambda usuario: usuario.serialize(),all_people)) 
+    return jsonify(results), 200
+
+@app.route('/people/<int:person_id>', methods=['GET'])
+def get_people_id(person_id):
+    print(person_id)
+    person = People.query.filter_by(id=person_id).first() 
+
+    return jsonify(person.serialize()), 200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    all_planets = Planets.query.all()
+    results = list(map(lambda usuario: usuario.serialize(),all_planets)) 
+    return jsonify(results), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_plnnet_id(planet_id):
+    print(planet_id)
+    planet = Planets.query.filter_by(id=planet_id).first() 
+
+    return jsonify(planet.serialize()), 200
+
+
+USER_ID = 1  
+
+@app.route('/user/favorites', methods=['GET'])
+def get_user_favorites():
+    favorite_people = FavoritePeople.query.all()
+    favorite_planets = FavoritePlanets.query.all()
+    people_results = list(map(lambda favorite: favorite.serialize(), favorite_people))
+    planets_results = list(map(lambda favorite: favorite.serialize(), favorite_planets))
+    results = {
+        "favorite_people": people_results,
+        "favorite_planets": planets_results
     }
+    
+    return jsonify(results), 200
 
-    return jsonify(response_body), 200
+
+
+@app.route('/favorite/planet/<int:planet_id>/<int:user_id>', methods=['POST'])
+def add_favorite_planet(planet_id, user_id):
+    planet = Planets.query.filter_by(id=planet_id).first()
+    if not planet:
+        return jsonify({'message': 'Planet not found'}), 404  
+    new_favorite = FavoritePlanets(user_id=user_id, planet_id=planet_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify(new_favorite.serialize()), 201 
+
+@app.route('/favorite/people/<int:people_id>/<int:user_id>', methods=['POST'])
+def add_favorite_people(people_id, user_id):
+    person = People.query.filter_by(id=people_id).first()
+    if not person:
+        return jsonify({'message': 'Person not found'}), 404  
+    new_favorite = FavoritePeople(user_id=user_id, people_id=people_id)
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify(new_favorite.serialize()), 201 
+
+@app.route('/favorite/planet/<int:planet_id>/<int:user_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id, user_id):
+    favorite = FavoritePlanets.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({'message': 'Favorite planet deleted'}), 200
+    return jsonify({'message': 'Favorite planet not found'}), 404
+
+@app.route('/favorite/people/<int:people_id>/<int:user_id>', methods=['DELETE'])
+def delete_favorite_people(people_id, user_id):
+    favorite = FavoritePeople.query.filter_by(user_id=user_id, people_id=people_id).first()
+    if favorite:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({'message': 'Favorite person deleted'}), 200
+    return jsonify({'message': 'Favorite person not found'}), 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,11 +2,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    lastname = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites_people = db.relationship('FavoritePeople', backref='user', lazy=True)
+    favorite_planets = db.relationship('FavoritePlanets', backref='user', lazy=True)
+    
+   
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +24,83 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "name": self.name,
+            "lastname": self.lastname,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    birth_year = db.Column(db.String(120), unique=False, nullable=False)
+    gender = db.Column(db.String(120), unique=False, nullable=False)
+    eye_color = db.Column(db.String(80), unique=False, nullable=False)
+    homeworld = db.Column(db.String(120), unique=False, nullable=False)
+    favorite_people = db.relationship('FavoritePeople', backref='people', lazy=True)
+
+
+    def __repr__(self):
+        return '<Person %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "eye_color": self.eye_color,
+            "homeworld": self.homeworld,
+            # do not serialize the password, its a security breach
+        }
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    climate = db.Column(db.String(120), unique=False, nullable=False)
+    terrain = db.Column(db.String(120), unique=False, nullable=False)
+    population = db.Column(db.String(80), unique=False, nullable=False)
+
+    favorite_planets = db.relationship('FavoritePlanets', backref='planet', lazy=True)
+
+    def __repr__(self):
+        return '<Person %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population,
+            # do not serialize the password, its a security breach
+        }
+    
+
+class FavoritePeople(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            
+        }
+
+class FavoritePlanets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            
         }
